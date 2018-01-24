be12c251b4acdb5ad6bc6f6b6afae6af84e9c1e4
Gavin Romig-Koch <gavin@redhat.com>
2018-01-24 11:48:03 -0500

    install ansible from EPEL if not registered

--- support-scripts/rhel7-install-ansible.sh
+++ support-scripts/rhel7-install-ansible.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-# RHEL7 ansible is in 'extras'
-subscription-manager repos --enable=rhel-7-server-extras-rpms
-yum install -y ansible
+# RHEL7 ansible is in 'extras' so use that unless we are not registered
+if subscription-manager status; then
+    subscription-manager repos --enable=rhel-7-server-extras-rpms
+    yum install -y ansible
+else
+    yum install -y https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+    yum install -y ansible
+fi
